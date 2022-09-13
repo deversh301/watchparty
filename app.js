f86b7254686fe011a1e6f2247653a204117d3979
Shubham yadav <sfs.shubham21@gmail.com>
2022-09-13 23:33:33 +0530

    first commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors())
 const { Server } = require("socket.io")
 const io = new Server(server ,  {
   cors: {
-    origin: ["http://192.168.1.4:3000", "http://localhost:3000"],
+    origin: ["http://192.168.1.4:3000", "http://watchpartyyoutube.epizy.com","http://localhost:3000"],
     // or with an array of origins
     // origin: ["https://my-frontend.com", "https://my-other-frontend.com", "http://localhost:3000"],
     credentials: true
