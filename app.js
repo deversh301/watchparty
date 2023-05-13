f93a41bd57f68e84c9c4c07a06e2ad4fefd0b878
Shubham Yadav <60321221+deversh301@users.noreply.github.com>
2023-05-13 16:16:18 +0000

    server

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors())
 const { Server } = require("socket.io")
 const io = new Server(server ,  {
   cors: {
-    origin: ["http://127.0.0.1:3000", "https://deversh301-animated-invention-5rpwxg9pg5gfqgw-3000.preview.app.github.dev/", "http://192.168.1.4:3000" , "http://192.168.1.3:3000" ,"http://watchpartyyoutube.epizy.com","http://localhost:3000"],
+    origin: ["/*"],
     // or with an array of origins
     // origin: ["https://my-frontend.com", "https://my-other-frontend.com", "http://localhost:3000"],
     credentials: true
